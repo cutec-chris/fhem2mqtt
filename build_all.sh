7ef4bb59aa9d4de65dda0821c805d9e4b247139b
Christian U <info@cu-tec.de>
2015-04-06 14:17:03 +0200

    install

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,58 @@
-zip fhem_to_mqtt_win32.zip output/i386-win32/fhem2mqtt.exe
-cd output/x86_64-linux
-tar -czf fhem_to_mqtt_linux64.tar.gz fhem2mqtt
+Program=fhem2mqtt
+Archfpc=$1
+if [ "x$Archfpc" = "x" ]; then
+  Arch=`dpkg --print-architecture`
+  Archfpc=$(fpc -h | grep 'Compiler version' | sed 's/.*for \([^ ]\+\)$/\1/')
+fi
+if [ "x$Arch" = "x" ]; then
+  if [ "x$Archfpc" = "xx86_64" ]; then
+    Arch=amd64
+  fi
+  if [ "x$Arch" = "x" ]; then
+    Arch=$Archfpc
+  fi
+fi
+sudo -S echo "Arch is $Arch"
+echo "Archfpc is $Archfpc"
+Year=`date +%y`
+Month=`date +%m`
+Day=`date +%d`
+Date=20$Year$Month$Day
+TmpDir=/tmp
+BuildDir=$TmpDir/software_build
+
+cd output/i386-win32/
+zip ../fhem_to_mqtt_win32.zip fhem2mqtt.exe
 cd ../..
-cd output/armhf-linux
-tar -czf fhem_to_mqtt_raspberry.tar.gz fhem2mqtt
-cd ../..
+
+rm -rf $BuildDir
+mkdir -p $BuildDir/usr/share/doc/$Program/
+echo "copyright and changelog files..."
+cp debian/changelog.Debian $BuildDir/usr/share/doc/$Program/
+cp changes.txt $BuildDir/usr/share/doc/$Program/changelog
+cp debian/copyright $BuildDir/usr/share/doc/$Program/
+gzip --best $BuildDir/usr/share/doc/$Program/changelog
+gzip --best $BuildDir/usr/share/doc/$Program/changelog.Debian
+chmod 644 $BuildDir/usr/share/doc/$Program/*
+echo "creating installation..."
+mkdir -p $BuildDir/usr/bin/
+echo "copy to builddir..."
+cp output/$Archfpc-linux/$Program $BuildDir/usr/bin/$Program
+DebSize=$(du -s $BuildDir | cut -f1)
+echo "fixing permissions ..."
+sudo -S find $BuildDir -type d -print0 | xargs -0 sudo -S chmod 755  # this is needed, don't ask me why
+sudo -S find $BuildDir -type f -print0 | xargs -0 sudo -S chmod a+r  # this is needed, don't ask me why
+sudo -S chown -hR root:root $BuildDir/usr
+echo "creating control file..."
+mkdir -p $BuildDir/DEBIAN
+cat debian/control | \
+  sed -e "s/VERSION/$Date/g" \
+      -e "s/ARCH/$Arch/g" \
+      -e "s/DEBSIZE/$DebSize/g" \
+  > $BuildDir/DEBIAN/control
+chmod 755 $BuildDir/DEBIAN
+echo "building package..."
+sudo -S dpkg-deb --build $BuildDir
+cp $TmpDir/software_build.deb output/${Program}_${Date}_${Arch}.deb
+echo "cleaning up..."
+sudo -S rm -r $BuildDir
